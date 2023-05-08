9442b0a366bb41956ccd9e9b456465be5f7e154c
SeongYoungJu <xtotoroz3@gmail.com>
2023-05-08 17:30:27 +0900

    백준 'card2' 문제 - 실패

--- card2-1.js
+++ card2-1.js
@@ -9,6 +9,11 @@ let input = fs.readFileSync("./input-card2.txt").toString();
 let inputNum = +input;
 // ------------------------- //
 
+// 입력되는 데이터의 숫자가 클수록 for문을 도는 데 많은 시간이 들고 많은 메모리를 잡아먹는다.
+// 문제에 제시된 메모리 제한은 128MB
+// n의 크기는 1 ~ 500,000
+// 자바스크립트는 숫자형 데이터의 경우 number 하나 당 8bytes의 공간을 확보한다.
+// 즉, n의 크기가 커지면 커질수록 메모리 낭비가 엄청 심해진다.
 function solution(n) {
     let count = 0;
     let numArr = [];
@@ -24,16 +29,9 @@ function solution(n) {
         }
 
         if (count % 2 !== 0) {
-            let tempArr = [];
             let tempNum = numArr[0];
 
-            for (let j = 0; j < numArr.length; j++) {
-                if (numArr[j] !== numArr[0]) {
-                    tempArr.push(numArr[j]);
-                }
-            }
-
-            numArr = [...tempArr, tempNum];
+            numArr = [...numArr.splice(1), tempNum];
             count += 1;
         }
     }
